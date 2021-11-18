835c22b1a4ac0846cd11e716f784fda0753c455e
mario carrillo <carrillo@rhrk.uni-kl.de>
2021-11-18 13:36:07 +0100

    Add initial templates for exercise 03
    These are some initial code given by Professor and
    scripts to build them.
    
    Signed-off-by: mario carrillo <carrillo@rhrk.uni-kl.de>

---
+++ exercise_03/kernel_p1.cu
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <helper_cuda.h>
+ 
+__global__ void kernel(uchar3 *pos,int width, int height) {
+	// Implement the kernel here
+}
+void simulate_p1(uchar3 *ptr, int tick, int w, int h)
+{
+	cudaError_t err=cudaSuccess;
+
+	// set number of threads/blocks
+	dim3 block(1,1,1);
+	dim3 threads(1,1,1);
+
+	// call your kernel
+	kernel<<< block,threads>>> (ptr,w,h);
+	err=cudaGetLastError();
+	if(err!=cudaSuccess) {
+		fprintf(stderr,"Error executing the kernel - %s\n",
+				 cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+	//  stop in the GL-Loop to look at picture
+	if(tick>=1) {
+		//getchar();
+	}
+}
