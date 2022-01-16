427ee42d633226edd531bc1e689d13cd82f47b0c
Yunmin Cho <cho@rhrk.uni-kl.de>
2022-01-16 02:26:00 +0100

    exercise07 - not finished

---
+++ exercise_07/E07_particles/README.md
@@ -0,0 +1,29 @@
+# Exercise 4
+
+## 1. HOWTO
+
+We can build and execute our code with default values  by following the next
+steps:
+
+```
+./access_to_node.sh
+make
+./exercise04
+```
+
+The default values are **80k** particles and **1000** kernel calls. However,
+if you would like to execute with different values you can use the flag `-p` to
+specify the number of particles and `-i` to specify the number of kernel calls.
+E.g.
+
+```
+./exercise04 -p 5000 -i 200
+```
+
+In addition, we can build and execute the binary to get device properties by
+following the next steps:
+
+```
+ make extras
+ ./DevProperties
+```
---
+++ exercise_07/E07_particles/main.cu
@@ -0,0 +1,212 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <iomanip>
+#include <math.h>
+#include <unistd.h>
+#include <helper_cuda.h>
+#include <sys/time.h>
+
+#define DEFAULT_NUM_ITERATIONS 1000
+#define DEFAULT_NUM_PARTICLES  1000000
+#define DEFAULT_NUM_TO_SHOW    10
+
+using namespace std;
+
+struct p {
+    float x;
+    float y;
+    float z;
+    float m;
+};
+
+static const char help[] =
+    "Usage: exercise01 [-k number] [-i number] [-p number] [-h]\n"
+    "Description:\n"
+    "  -i number:     Specifies how many times the kernel will be\n"
+    "                 executed.\n"
+    "  -p number:     Number of particles to be processed\n"
+    "  -h             Prints this help message.\n";
+
+void Print(p *x) {
+    for (int i = 0; i < DEFAULT_NUM_TO_SHOW; i++)
+        cout << x[i].x << endl;
+}
+
+void init(p *xin, long npart) {
+    for (int i = 0; i < npart; i++) {
+        xin[i].x = (float(rand())/float((RAND_MAX)) * 10.0f) + 0.1f;
+        xin[i].y = (float(rand())/float((RAND_MAX)) * 10.0f) + 0.1f;
+        xin[i].z = (float(rand())/float((RAND_MAX)) * 10.0f) + 0.1f;
+        xin[i].m = (float(rand())/float((RAND_MAX)) * 10.0f) + 0.1f;
+    }
+}
+
+// GPU Kernel
+__global__ void kernel(p *xin, p *xout, long int npart, double dt, double val) {
+    int t = threadIdx.x;
+    int g = blockIdx.x;
+    //int i = t + g * blockDim.x;
+    //int off = gridDim.x * blockDim.x;
+    int maxrad = 1.0;
+    float f, dsq;
+    float distx, disty, distz;
+    int size = blockDim.x * 5;
+
+    extern __shared__ p *x_shared;
+    p *xi = &x_shared[0];
+    *((float4*)(&xi[0]) + t) = *((float4*)(&xin[g * blockDim.x]) + t);
+
+    //extern __shared__ p xj[size];
+    p *xj = &x_shared[blockDim.x];
+    for(int ja = 0; ja < npart; ja+= size) {
+        for(int jl = 0; jl < size / blockDim.x; jl += blockDim.x){
+            *((float4*)(&xj[jl]) + t) = *((float4*)(&xj[ja + jl]) + t);
+        }
+        __syncthreads();
+
+        for(int j = ja; j < ja + size; j++){
+            distx = xi[t].x - xj[j - ja].x;
+            disty = xi[t].y - xj[j - ja].y;
+            distz = xi[t].z - xj[j - ja].z;
+
+            dsq = distx * distx + disty * disty + distz * distz;
+
+            if (dsq < maxrad && dsq != 0 && t != j) {
+                f += xi[t].m * xj[j - ja].m * (xi[t].x - xj[j - ja].x) / dsq;
+            }
+        }
+    }
+    double s = f * dt * val;
+    xout[t].x = xi[t].x + s;
+    xout[t].y = xi[t].y + s;
+    xout[t].z = xi[t].z + s;
+
+    /*
+    while (i < npart) {
+        xout[i].x = xin[i].x;
+        xout[i].y = xin[i].y;
+        xout[i].z = xin[i].z;
+        f = 0.0f;
+        dsq = 0.0f;
+        distx = 0.0f;
+        disty = 0.0f;
+        distz = 0.0f;
+        for (int j = 0; j < npart; j++) {
+            distx = xin[i].x - xin[j].x;
+            disty = xin[i].y - xin[j].y;
+            distz = xin[i].z - xin[j].z;
+
+            dsq = distx * distx + disty * disty + distz * distz;
+
+            if (dsq < maxrad && dsq != 0 && i != j) {
+                f += xin[i].m * xin[j].m * (xin[i].x - xin[j].x) / dsq;
+            }
+        }
+        double s = f * dt * val;
+        xout[i].x += s;
+        xout[i].y += s;
+        xout[i].z += s;
+
+        i+=off;
+    }
+    */
+}
+
+
+void execute_kernel(p *xin, p *xout, int npart, int niters) {
+    p *x_dev;
+    p *xin_dev;
+    p *xout_dev;
+    float dt = 0.5f;
+    float val = 0.5f;
+    float execution_time = 0.0f;
+
+    // Structures to measure time
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // Set number of threads/blocks
+    dim3 block(4096, 1, 1);
+    dim3 threads(1024, 1, 1);
+
+     // START measure time
+    cudaEventRecord(start, 0);
+
+    // Memory management
+    checkCudaErrors(cudaMalloc((void **)&xin_dev, sizeof(p) * npart));
+    checkCudaErrors(cudaMalloc((void **)&xout_dev, sizeof(p) * npart));
+    checkCudaErrors(cudaMemcpy(xin_dev, xin, sizeof(p) * npart, cudaMemcpyHostToDevice));
+
+    // for dynamic allocation of shared memory
+    // Kernel 1 execution
+    for (int i = 0; i < niters; i++) {
+        kernel<<<block, threads, sizeof(p) * 4096 * 6>>>(xin_dev, xout_dev, npart, dt, val);
+
+        // Exchange pointers
+        x_dev = xin_dev;
+        xin_dev = xout_dev;
+        xout_dev = x_dev;
+    }
+
+    checkCudaErrors(cudaMemcpy(xout, xout_dev, sizeof(p) * npart, cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaFree(xin_dev));
+    checkCudaErrors(cudaFree(xout_dev));
+
+    // STOP measure time
+    cudaEventRecord(stop, 0);
+
+    // Calculate time
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&execution_time, start, stop);
+    printf("kernel execution: %f seconds\n", (execution_time / 1000.0f));
+}
+
+
+int exercise04(int npart, int niters) {
+    p *xin = (p *)malloc(sizeof(p) * npart);
+    p *xout = (p *)malloc(sizeof(p) * npart);
+    init(xin, npart);
+    execute_kernel(xin, xout, npart, niters);
+    free(xin);
+    free(xout);
+
+    return 0;
+}
+
+
+int main(int argc, char **argv) {
+    int opt;
+    int niters = DEFAULT_NUM_ITERATIONS;
+    long int npart = DEFAULT_NUM_PARTICLES;
+
+    while ((opt = getopt(argc, argv, "i:p:h")) != EOF) {
+        switch (opt) {
+            case 'i':
+                niters = atoi(optarg);
+                break;
+            case 'p':
+                npart = atoi(optarg);
+                break;
+            case 'h':
+                cout << help << endl;
+                return 0;
+            case '?':
+                cerr << "error: unknown option" << endl;
+                cout << help << endl;
+                return 1;
+            default:
+                cerr << help << endl;
+                return 1;
+        }
+    }
+
+    cout << "Particles: " << npart << endl;
+    cout << "Iterations: " << niters << endl;
+    cout << "Executing ..." << endl;
+    exercise04(npart, niters);
+
+    return 0;
+}
+
