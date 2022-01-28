737ba47dfe0308d10882c75f21f2c306570403a7
mario carrillo <carrillo@rhrk.uni-kl.de>
2022-01-28 19:44:47 +0100

    Fix exercise 04 implementation
    The exercise 04 was wrong implemented. The commit removes the
    the interation over structures and replace them by linear arrays.
    In addition, the blocks and threads are computed according to
    the number of particles. Finally, it removes the unecessary
    while loop and the iterations over off incerment. The execution
    time was reduced from 771 seconds to 175 seconds approximatly.
    
    Signed-off-by: mario carrillo <carrillo@rhrk.uni-kl.de>

--- exercise_04/main.cu
+++ exercise_04/main.cu
@@ -11,129 +11,223 @@
 #define DEFAULT_NUM_PARTICLES  80000
 #define DEFAULT_NUM_TO_SHOW    10
 
+// This number was obtained by checking the device properties
+// of a P2000 card. This project includes a script to use it.
+#define MAX_THREADS_PER_BLOCK 1024
+
 using namespace std;
 
-struct p {
-    float x;
-    float y;
-    float z;
-    float m;
-};
-
 static const char help[] =
-    "Usage: exercise01 [-k number] [-i number] [-p number] [-h]\n"
+    "Usage: exercise04 [-k number] [-i number] [-p number] [-h]\n"
     "Description:\n"
     "  -i number:     Specifies how many times the kernel will be\n"
     "                 executed.\n"
     "  -p number:     Number of particles to be processed\n"
     "  -h             Prints this help message.\n";
 
-void Print(p *x) {
-    for (int i = 0; i < DEFAULT_NUM_TO_SHOW; i++)
-        cout << x[i].x << endl;
+void Print(float *arr) {
+    for (int i = 0; i < DEFAULT_NUM_TO_SHOW; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
-void init(p *xin, long npart) {
+void init(float *arr, long npart) {
     for (int i = 0; i < npart; i++) {
-        xin[i].x = (float(rand())/float((RAND_MAX)) * 10.0f) + 0.1f;
-        xin[i].y = (float(rand())/float((RAND_MAX)) * 10.0f) + 0.1f;
-        xin[i].z = (float(rand())/float((RAND_MAX)) * 10.0f) + 0.1f;
-        xin[i].m = (float(rand())/float((RAND_MAX)) * 10.0f) + 0.1f;
+        arr[i] = (float(rand())/float((RAND_MAX)));
     }
 }
 
 // GPU Kernel
-__global__ void kernel(p *xin, p *xout, long int npart, double dt, double val) {
+__global__ void kernel(float *xin, float *yin, float *zin,
+                        float *xout, float *yout, float *zout,
+                        float *m, long int npart, double dt, double val) {
     int t = threadIdx.x;
     int g = blockIdx.x;
     int i = t + g * blockDim.x;
-    int off = gridDim.x * blockDim.x;
-    int maxrad = 1.0;
-    float f, dsq;
+    float maxrad = 0.9f;
+    float f = 0.0f;
 
-    while (i < npart) {
-        xout[i].x = xin[i].x;
-        xout[i].y = xin[i].y;
-        xout[i].z = xin[i].z;
-        f = 0.0f;
-        dsq = 0.0f;
+    // distance vars
+    float dsq;
+    float dstx;
+    float dsty;
+    float dstz;
+
+    if (i < npart) {
+        xout[i] = xin[i];
+        yout[i] = yin[i];
+        zout[i] = zin[i];
+
         for (int j = 0; j < npart; j++) {
-            dsq = (
-                    powf(xin[i].x - xin[j].x, 2.0f) +
-                    powf(xin[i].y - xin[j].y, 2.0f) +
-                    powf(xin[i].x - xin[j].x, 2.0f)
-            );
+            dstx = xin[i] - xin[j];
+            dsty = yin[i] - yin[j];
+            dstz = zin[i] - zin[j];
+            dsq =  (dstx * dstx) + (dsty * dsty) + (dstz * dstz);
 
             if (dsq < maxrad && dsq != 0 && i != j) {
-                f += xin[i].m * xin[j].m * (xin[i].x - xin[j].x) / dsq;
+                f += m[i] * m[j] * (xin[i] - xin[j]) / dsq;
             }
         }
-        double s = f * dt * val;
-        xout[i].x += s;
-        xout[i].y += s;
-        xout[i].z += s;
 
-        i+=off;
+        float s = f * dt * val;
+        xout[i] += s;
+        yout[i] += s;
+        zout[i] += s;
     }
 }
 
 
-void execute_kernel(p *xin, p *xout, int npart, int niters) {
-    p *x_dev;
-    p *xin_dev;
-    p *xout_dev;
-    float dt = 0.5f;
-    float val = 0.5f;
+int exercise04(int npart, int niters) {
+    // CPU vars
+    float *xin, *yin, *zin;
+    float *xout, *yout, *zout;
+    float *m;
+
+    // GPU vars
+    float *xin_dev, *yin_dev, *zin_dev;
+    float *xout_dev, *yout_dev, *zout_dev;
+    float *m_dev;
+
+    // Exchange vars
+    float *x_dev, *y_dev, *z_dev;
+
+    // kernel parameters
+    float dt = 0.2f;
+    float val = 0.2f;
     float execution_time = 0.0f;
+
+    // CPU memory allocations inputs
+    xin = (float*)malloc(npart * sizeof(float));
+    yin = (float*)malloc(npart * sizeof(float));
+    zin = (float*)malloc(npart * sizeof(float));
+    m = (float*)malloc(npart * sizeof(float));
+
+    // CPU memory allocation  outputs
+    xout = (float*)malloc(npart * sizeof(float));
+    yout = (float*)malloc(npart * sizeof(float));
+    zout = (float*)malloc(npart * sizeof(float));
+
+    // Initialize particles with random values
+    init(xin, npart);
+    init(yin, npart);
+    init(zin, npart);
+    init(m, npart);
+
+    // GPU memory allocation inputs
+    checkCudaErrors(cudaMalloc((void **)&xin_dev, npart * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **)&yin_dev, npart * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **)&zin_dev, npart * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **)&m_dev, npart * sizeof(float)));
+
+    // GPU memory allocation outputs
+    checkCudaErrors(cudaMalloc((void **)&xout_dev, npart * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **)&yout_dev, npart * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **)&zout_dev, npart * sizeof(float)));
 
     // Structures to measure time
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    // Set number of threads/blocks
-    dim3 block(4096, 1, 1);
-    dim3 threads(1024, 1, 1);
+    // Calculate blocks & threads
+    int minimum_blocks;
+    int minimum_threads;
+    int extra_block;
+    int total_blocks;
 
-     // START measure time
-    cudaEventRecord(start, 0);
-
-    // Memory management
-    checkCudaErrors(cudaMalloc((void **)&xin_dev, sizeof(p) * npart));
-    checkCudaErrors(cudaMalloc((void **)&xout_dev, sizeof(p) * npart));
-    checkCudaErrors(cudaMemcpy(xin_dev, xin, sizeof(p) * npart, cudaMemcpyHostToDevice));
-
-    // Kernel 1 execution
-    for (int i = 0; i < niters; i++) {
-        kernel<<<block, threads>>>(xin_dev, xout_dev, npart, dt, val);
-
-        // Exchange pointers
-        x_dev = xin_dev;
-        xin_dev = xout_dev;
-        xout_dev = x_dev;
+    // Minimum number of blocks/threads calculation
+    if (npart < MAX_THREADS_PER_BLOCK) {
+        minimum_blocks = 1;
+        minimum_threads = npart;
+    } else {
+        minimum_blocks = npart / MAX_THREADS_PER_BLOCK;
+        minimum_threads = MAX_THREADS_PER_BLOCK;
     }
 
-    checkCudaErrors(cudaMemcpy(xout, xout_dev, sizeof(p) * npart, cudaMemcpyDeviceToHost));
-    checkCudaErrors(cudaFree(xin_dev));
-    checkCudaErrors(cudaFree(xout_dev));
+    // Extra block calculation
+    if (npart % MAX_THREADS_PER_BLOCK == 0 || npart < MAX_THREADS_PER_BLOCK)
+        extra_block = 0;
+    else
+        extra_block = 1;
+
+    total_blocks = minimum_blocks + extra_block;
+    dim3 blocks(total_blocks, 1, 1);
+    dim3 threads(minimum_threads, 1, 1);
+    printf("Blocks:%d   Threads:%d\n", total_blocks, minimum_threads);
+
+    // Debug
+    /*
+ *  printf("Sampling INPUT\n");
+    Print(xin);
+    */
+
+    // START measure time
+    cudaEventRecord(start, 0);
+
+    // Copy data from CPU to GPU
+    checkCudaErrors(cudaMemcpy(xin_dev, xin, npart * sizeof(float), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(yin_dev, yin, npart * sizeof(float), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(zin_dev, zin, npart * sizeof(float), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(m_dev, m, npart * sizeof(float), cudaMemcpyHostToDevice));
+
+    // Kernel execution
+    for (int i = 0; i < niters; i++) {
+        kernel<<<blocks, threads>>>(
+                        xin_dev, yin_dev, zin_dev,
+                        xout_dev, yout_dev, zout_dev,
+                        m_dev, npart, dt, val
+        );
+        cudaDeviceSynchronize();
+
+        // Data exchange
+        x_dev = xin_dev;
+        y_dev = yin_dev;
+        z_dev = zin_dev;
+        xin_dev = xout_dev;
+        yin_dev = yout_dev;
+        zin_dev = zout_dev;
+        xout_dev = x_dev;
+        yout_dev = y_dev;
+        zout_dev = z_dev;
+    }
+
+    // Copy data from GPU to CPU
+    checkCudaErrors(cudaMemcpy(xout, xout_dev, npart * sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(yout, yout_dev, npart * sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(zout, zout_dev, npart * sizeof(float), cudaMemcpyDeviceToHost));
 
     // STOP measure time
     cudaEventRecord(stop, 0);
+
+    // Debug
+    /*
+    printf("Sampling OUTPUT\n");
+    Print(xout);
+    */
 
     // Calculate time
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&execution_time, start, stop);
     printf("kernel execution: %f seconds\n", (execution_time / 1000.0f));
-}
 
+    // Free GPU memory allocations
+    checkCudaErrors(cudaFree(xin_dev));
+    checkCudaErrors(cudaFree(yin_dev));
+    checkCudaErrors(cudaFree(zin_dev));
+    checkCudaErrors(cudaFree(xout_dev));
+    checkCudaErrors(cudaFree(yout_dev));
+    checkCudaErrors(cudaFree(zout_dev));
+    checkCudaErrors(cudaFree(m_dev));
 
-int exercise04(int npart, int niters) {
-    p *xin = (p *)malloc(sizeof(p) * npart);
-    p *xout = (p *)malloc(sizeof(p) * npart);
-    init(xin, npart);
-    execute_kernel(xin, xout, npart, niters);
+    // Free memory
     free(xin);
+    free(yin);
+    free(zin);
     free(xout);
+    free(yout);
+    free(zout);
+    free(m);
 
     return 0;
 }
